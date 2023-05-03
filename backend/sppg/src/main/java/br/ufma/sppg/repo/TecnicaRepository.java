33a39d035ca87665d5c9572065a68cd81b6740f5
luisEscm <130574646+luisEscm@users.noreply.github.com>
2023-05-03 17:16:58 -0300

    Add files via upload

--- backend/sppg/src/main/java/br/ufma/sppg/repo/TecnicaRepository.java
+++ backend/sppg/src/main/java/br/ufma/sppg/repo/TecnicaRepository.java
@@ -14,23 +14,4 @@ public interface TecnicaRepository extends JpaRepository<Tecnica, Long> {
     Optional<Tecnica> findById(Integer idTecnica);
     Boolean existsById(Integer idTecnica);
     
-    @Modifying
-    @Transactional
-    @Query("delete from Tecnica t where (t.idTecnica = :idTec and t.orientacoes is empty)")
-    void remover(@Param("idTec") Integer idTecnica);
-
-    @Transactional
-    @Modifying()
-    @Query("update Tecnica t set t.qtdGrad = :qtd_Grad where t.id = :id_Tecnica")
-    void atualizarQtdGrad(@Param("id_Tecnica") Integer idTecnica, @Param("qtd_Grad") Integer qtdGrad);
-
-    @Transactional
-    @Modifying()
-    @Query("update Tecnica t set t.qtdMestrado = :qtd_Mestrado where t.id = :id_Tecnica")
-    void atualizarQtdMestrado(@Param("id_Tecnica") Integer idTecnica, @Param("qtd_Mestrado") Integer qtdMestrado);
-
-    @Transactional
-    @Modifying()
-    @Query("update Tecnica t set t.qtdDoutorado = :qtd_Doutorado where t.id = :id_Tecnica")
-    void atualizarQtdDoutorado(@Param("id_Tecnica") Integer idTecnica, @Param("qtd_Doutorado") Integer qtdDoutorado);
 }

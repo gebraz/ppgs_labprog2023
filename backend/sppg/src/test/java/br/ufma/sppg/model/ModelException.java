c388f251f3b79a0b5d78a66b7d419203d3ed9acb
qualquer <felipersteles@gmail.com>
2023-05-03 16:45:12 -0300

    remove: model excecption

--- backend/sppg/src/test/java/br/ufma/sppg/model/ModelException.java
+++
@@ -1,9 +0,0 @@
-package br.ufma.sppg.model;
-
-public class ModelException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public ModelException(String message) {
-        super(message);
-    }
-}

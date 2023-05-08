1ad0bd5a4e3bae3fb0936e9fa0fc9cbcfb15bb03
Hiago Antonio Costa Silva <71812623+HiagoACSilva@users.noreply.github.com>
2023-05-08 16:15:09 -0300

    Add files via upload

---
+++ backend/sppg/src/main/java/br/ufma/sppg/repo/OrientacaoRepository.java
@@ -0,0 +1,10 @@
+package br.ufma.sppg.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufma.sppg.model.Orientacao;
+
+public interface OrientacaoRepository 
+    extends JpaRepository<Orientacao, Integer>{
+    
+}
---
+++ backend/sppg/src/main/java/br/ufma/sppg/repo/ProducaoRepository.java
@@ -0,0 +1,10 @@
+package br.ufma.sppg.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufma.sppg.model.Producao;
+
+public interface ProducaoRepository 
+    extends JpaRepository<Producao, Integer>{
+    
+}

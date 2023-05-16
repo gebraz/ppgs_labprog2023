d4a6a27295f94ff4d6f9e42f4a29e7da30db087f
luisEscm <130574646+luisEscm@users.noreply.github.com>
2023-05-16 20:49:59 -0300

    Add files via upload

--- backend/sppg/src/main/java/br/ufma/sppg/repo/DocenteRepository.java
+++ backend/sppg/src/main/java/br/ufma/sppg/repo/DocenteRepository.java
@@ -1,11 +1,15 @@
 package br.ufma.sppg.repo;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.ufma.sppg.model.Docente;
-                                                //Tipo do repository e Tipo do ID
-public interface DocenteRepository extends JpaRepository<Docente, Integer> {
-    List<Docente> findByNome(String nome);
+
+public interface DocenteRepository 
+    extends JpaRepository<Docente, Integer>{
+    
+    Optional<Docente> findById(Integer idDocente);
+
+    boolean existsById(Integer idDocente);
 }

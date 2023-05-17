375df25121c432673eebef52eae4ac8008d1aed8
John <jovijovi2019@hotmail.com>
2023-05-17 11:04:26 -0300

    feat: Salvar Tecnica

--- backend/sppg/src/main/java/br/ufma/sppg/service/TecnicaService.java
+++ backend/sppg/src/main/java/br/ufma/sppg/service/TecnicaService.java
@@ -1,13 +1,17 @@
 package br.ufma.sppg.service;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.ufma.sppg.model.Docente;
+import br.ufma.sppg.model.Orientacao;
 import br.ufma.sppg.model.Tecnica;
 import br.ufma.sppg.repo.TecnicaRepository;
 import br.ufma.sppg.service.exceptions.RegrasRunTime;
+import jakarta.transaction.Transactional;
 
 @Service
 public class TecnicaService {
@@ -15,6 +19,11 @@ public class TecnicaService {
   @Autowired
   TecnicaRepository tecnicaRepo;
 
+  public Tecnica salvarTecnica(Tecnica tecnica) {
+    return tecnicaRepo.save(tecnica);
+  }
+
+  @Transactional
   public Tecnica atualizarEstatisticas(Integer idTecnica, Integer qtdGrad, Integer qtdMestrado, Integer qtdDoutorado) {
 
     Optional<Tecnica> tecnica = tecnicaRepo.findById(idTecnica);
@@ -32,4 +41,37 @@ public class TecnicaService {
     throw new RegrasRunTime("A técnica informada não existe!");
   }
 
+  public ArrayList<Orientacao> obterOrientacoesTecnica(Integer idTecnica) {
+    ArrayList<Orientacao> orientacoes = new ArrayList<>();
+
+    Optional<Tecnica> tecnica = tecnicaRepo.findById(idTecnica);
+
+    if (tecnica.isPresent()) {
+
+      for (Orientacao orientacao : tecnica.get().getOrientacoes()) {
+        orientacoes.add(orientacao);
+      }
+
+      return orientacoes;
+    }
+
+    throw new RegrasRunTime("A técnica informada não existe");
+  }
+
+  public ArrayList<Docente> obterDocentesTecnica(Integer idTecnica) {
+    ArrayList<Docente> docentes = new ArrayList<>();
+
+    Optional<Tecnica> tecnica = tecnicaRepo.findById(idTecnica);
+
+    if (tecnica.isPresent()) {
+
+      for (Docente docente : tecnica.get().getDocentes()) {
+        docentes.add(docente);
+      }
+
+      return docentes;
+    }
+
+    throw new RegrasRunTime("A técnica informada não existe");
+  }
 }

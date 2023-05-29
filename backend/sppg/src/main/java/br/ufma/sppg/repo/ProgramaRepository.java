db8d3b0bc52a8c710ff9b9dee4a8f3115328839b
Thalisson <55109282+thalissonjon@users.noreply.github.com>
2023-05-29 16:42:13 -0300

    Mudança repo programa

--- backend/sppg/src/main/java/br/ufma/sppg/repo/ProgramaRepository.java
+++ backend/sppg/src/main/java/br/ufma/sppg/repo/ProgramaRepository.java
@@ -16,16 +16,16 @@ public interface ProgramaRepository
     extends JpaRepository<Programa, Integer> {
         List<Programa> findAllByNome(String nomePg);
 
-        @Query("SELECT p FROM Programa p JOIN Docente o where o.id = :idPg")
+        @Query("SELECT u FROM Programa p JOIN p.docentes u where p.id = :idPg")
         List<Docente> obterDocentes(@Param("idPg") Integer idPg);
 
-        @Query("SELECT p FROM Programa p JOIN Docente d JOIN Producao o WHERE o.id = :idPg")
+        @Query("SELECT o FROM Programa p JOIN p.docentes j JOIN j.producoes o WHERE p.id = :idPg")
         List<Producao> obterallProducoes(@Param("idPg") Integer idPg);
 
-        @Query("SELECT p FROM Programa p JOIN Docente d JOIN Tecnica o WHERE o.id = :idPg")
+        @Query("SELECT t FROM Programa p JOIN p.docentes k JOIN k.tecnicas t WHERE p.id = :idPg")
         List<Tecnica> obterallTecnicas(@Param("idPg") Integer idPg);
     
-        @Query("SELECT p FROM Programa p JOIN Docente d JOIN Orientacao o WHERE o.id = :idPg")
+        @Query("SELECT s FROM Programa p JOIN p.docentes m JOIN m.orientacoes s WHERE p.id = :idPg")
         List<Orientacao> obterallOrientacoes(@Param("idPg") Integer idPg);
 
 }

5ae95f1d2e8a9ed9f42a138920c5085cd3ced62f
Felipe Teles <55213753+felipersteles@users.noreply.github.com>
2023-05-29 16:22:16 -0300

    Update OrientacaoRepository.java

--- backend/sppg/src/main/java/br/ufma/sppg/repo/OrientacaoRepository.java
+++ backend/sppg/src/main/java/br/ufma/sppg/repo/OrientacaoRepository.java
@@ -11,9 +11,9 @@ import br.ufma.sppg.model.Orientacao;
 public interface OrientacaoRepository extends JpaRepository<Orientacao, Integer> {
     List<Orientacao> findAllById(Integer id);
 
-    @Query("SELECT o FROM Orientacao o JOIN o.orientador d JOIN d.programas p where p.id = :idPrograma")
-    Optional<List<Orientacao>> findByPPG(Integer idPrograma);
+    @Query("SELECT o FROM Orientacao o JOIN o.orientador d JOIN d.programas p where p.id = :idPrograma AND o.ano >= :anoInicio AND o.ano<= :anoFim")
+    Optional<List<Orientacao>> findByPPG(Integer idPrograma, Integer anoInicio, Integer anoFim);
 
-    @Query("SELECT o FROM Orientacao o JOIN o.orientador d where d.id = :idDocente")
-    Optional<List<Orientacao>> findByDocente(Integer idDocente);
+    @Query("SELECT o FROM Orientacao o JOIN o.orientador d WHERE d.id = :idDocente AND o.ano >= :anoInicio AND o.ano<= :anoFim")
+    Optional<List<Orientacao>> findByDocente(Integer idDocente, Integer anoInicio, Integer anoFim);
 }

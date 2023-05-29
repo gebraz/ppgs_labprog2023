061e78a1f439b26b6feeb8d9d37af1dbf70b3776
Felipe Teles <felipersteles@gmail.com>
2023-05-29 12:22:58 -0300

    finish: Orientacao Service and Repository

--- backend/sppg/src/main/java/br/ufma/sppg/repo/OrientacaoRepository.java
+++ backend/sppg/src/main/java/br/ufma/sppg/repo/OrientacaoRepository.java
@@ -10,4 +10,10 @@ import br.ufma.sppg.model.Orientacao;
 
 public interface OrientacaoRepository extends JpaRepository<Orientacao, Integer> {
     List<Orientacao> findAllById(Integer id);
+
+    @Query("SELECT o FROM Orientacao o JOIN o.orientador d JOIN d.programas p where p.id = :idPrograma")
+    Optional<List<Orientacao>> findByPPG(Integer idPrograma);
+
+    @Query("SELECT o FROM Orientacao o JOIN o.orientador d where d.id = :idDocente")
+    Optional<List<Orientacao>> findByDocente(Integer idDocente);
 }

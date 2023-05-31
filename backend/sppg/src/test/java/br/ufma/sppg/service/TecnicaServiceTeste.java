90226462ad5482aecfb08fae2a92e860c1a17d3e
Luís Felipe <luisfrp741@gmail.com>
2023-05-30 21:46:39 -0300

    Teste de técnica da camada Service

--- backend/sppg/src/test/java/br/ufma/sppg/service/TecnicaServiceTeste.java
+++ backend/sppg/src/test/java/br/ufma/sppg/service/TecnicaServiceTeste.java
@@ -21,7 +21,7 @@ import br.ufma.sppg.repo.DocenteRepository;
 import br.ufma.sppg.repo.OrientacaoRepository;
 import br.ufma.sppg.repo.ProgramaRepository;
 import br.ufma.sppg.repo.TecnicaRepository;
-import br.ufma.sppg.service.exceptions.RegrasRunTime;
+import br.ufma.sppg.service.exceptions.ServicoRuntimeException;
 
 
 @SpringBootTest
@@ -51,9 +51,10 @@ public class TecnicaServiceTeste {
         return novoDocente;
     }
 
-    private Programa progFactory () throws ParseException{
+    private Programa progFactory (){
         Programa novoPrograma = Programa.builder()
         .nome("Programa de Pós-Graduação em Engenharia de Eletricidade - PPGEE")
+        .id(1)
         .build();
         return novoPrograma;
     }
@@ -64,7 +65,7 @@ public class TecnicaServiceTeste {
         return orientacao;
     }
 
-    private Tecnica tecnicaFactory()throws ParseException{
+    private Tecnica tecnicaFactory(){
         Tecnica novTecnica = Tecnica.builder().tipo("tipo1")
                                     .id(1)
                                     .titulo("titulo1")
@@ -83,16 +84,18 @@ public class TecnicaServiceTeste {
         // Cenario - cria / builder
         Tecnica tecnicaD = tecnicaFactory();
         Docente docente = docenteFactory();
-       
+        Docente docSalvo = repositoryDoc.save(docente);
+        Tecnica tecSalvo = repositoryTec.save(tecnicaD);
+
         //listas para salvar e setar em cada classe
         List<Docente> ldocente = new ArrayList<>();
         List<Tecnica> ltecnica = new ArrayList<>();
 
-        ldocente.add(repositoryDoc.save(docente));
-        ltecnica.add(repositoryTec.save(tecnicaD));
+        ldocente.add(docSalvo);
+        ltecnica.add(tecSalvo);
 
-        docente.setTecnicas(ltecnica);
-        tecnicaD.setDocentes(ldocente);
+        docSalvo.setTecnicas(ltecnica);
+        tecSalvo.setDocentes(ldocente);
 
         //acao
         Optional<List<Tecnica>> tec = service.obterTecnicasDocentePorPeriodo(docente.getId(), 2020, 2023) ;
@@ -100,7 +103,7 @@ public class TecnicaServiceTeste {
         //Verificação
         Assertions.assertNotNull(aux); 
         Assertions.assertFalse(aux.isEmpty()); 
-        //Assertions.assertEquals(ltecnica,aux);    
+        Assertions.assertEquals(ltecnica,aux);    
 
         repositoryTec.delete(tecnicaD);
         repositoryDoc.delete(docente);
@@ -110,18 +113,21 @@ public class TecnicaServiceTeste {
     public void deveGerarErroAoUsarIntervaloDeTempoComPeriodoInvalido() throws ParseException{
         Tecnica tecnica = tecnicaFactory();
         Docente docente = docenteFactory();
-       
+        Docente docSalvo = repositoryDoc.save(docente);
+        Tecnica tecSalvo = repositoryTec.save(tecnica);
+
+
         List<Docente> ldocente = new ArrayList<>();
         List<Tecnica> ltecnica = new ArrayList<>();
 
-        ldocente.add(repositoryDoc.save(docente));
-        ltecnica.add(repositoryTec.save(tecnica));
+        ldocente.add(docSalvo);
+        ltecnica.add(tecSalvo);
 
         docente.setTecnicas(ltecnica);
         tecnica.setDocentes(ldocente);
 
         //acao
-        Assertions.assertThrows(RegrasRunTime.class, 
+        Assertions.assertThrows(ServicoRuntimeException.class, 
                                     () -> service.obterTecnicasDocentePorPeriodo(docente.getId(), -1, 2023), 
                                     "Periodo inválido");
     }
@@ -153,6 +159,7 @@ public class TecnicaServiceTeste {
         //Verificação
         Assertions.assertNotNull(aux); 
         Assertions.assertFalse(aux.isEmpty());    
+        Assertions.assertTrue(aux.contains(ltecnica)); 
 
         repositoryTec.delete(tecnicaD);
         repositoryDoc.delete(docente);
@@ -186,11 +193,12 @@ public class TecnicaServiceTeste {
         docSalvo.setProgramas(lprog);
         tecSalvo.setDocentes(ldocente);
         progSalvo.setDocentes(ldocente);
+    
 
         //acao
         Optional<List<Tecnica>> tec = service.obterTecnicasPPGPorPeriodo(progSalvo.getId(), ini, fim);
         List<Tecnica> aux = tec.get();
-        //Verificação
+        // Verificação
         Assertions.assertEquals(ltecnica,aux);
         Assertions.assertEquals(aux.get(aux.size()).getId(),tecSalvo.getId());
         Assertions.assertNotNull(aux); 
@@ -209,7 +217,7 @@ public class TecnicaServiceTeste {
         tecnica.setQtdMestrado(null);
         tecnica.setQtdDoutorado(null); 
 
-        Assertions.assertThrows(RegrasRunTime.class, 
+        Assertions.assertThrows(ServicoRuntimeException.class, 
                                     () -> service.salvarTecnica(tecnica), 
                                     "Técnica com estatítiscas nulas");
     }
@@ -244,13 +252,15 @@ public class TecnicaServiceTeste {
         //Verificação
         Assertions.assertNotNull(aux); 
         Assertions.assertFalse(aux.isEmpty()); 
-        Assertions.assertEquals(lori,aux);    
-        Assertions.assertEquals(aux.get(aux.size()).getId(),oriSalvo.getId());
+        Assertions.assertEquals(lori,aux);
+          
+        // Assertions.assertEquals(aux.get(aux.size()).getId(),oriSalvo.getId());
  
 
         repositoryTec.delete(tecnicaD);
         repositoryDoc.delete(docente);
-        repositoryOri.delete(orientacoes);
+        //repositoryOri.delete(orientacoes);
     }
 
 }
+

8867e5e584266084f1bfbec488d7650a9a7c7340
Thalisson <55109282+thalissonjon@users.noreply.github.com>
2023-06-05 15:50:33 -0300

    endpoints gerais att

--- backend/sppg/src/main/java/br/ufma/sppg/controller/ProgramaController.java
+++ backend/sppg/src/main/java/br/ufma/sppg/controller/ProgramaController.java
@@ -17,6 +17,66 @@ public class ProgramaController {
     @Autowired
     ProgramaService servicePPG;
 
+    @Autowired
+    ProgramaService programa;
+
+    @GetMapping("/obterPrograma")
+    public ResponseEntity obterPrograma(
+            @RequestParam("programa") String nome){
+        try{
+            List <Programa> programas = programa.obterPrograma(nome);
+            return new ResponseEntity(programas, HttpStatus.OK);
+        }catch (ServicoRuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/obterDocentesPrograma")
+    public ResponseEntity obterDocentesPrograma(
+        @RequestParam("docente") Integer idPrograma){
+        try{
+            List <Docente> docentes = programa.obterDocentesPrograma(idPrograma);
+            return new ResponseEntity(docentes, HttpStatus.OK);
+        }catch (ServicoRuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/obterProducoesPrograma")
+    public ResponseEntity obterProducoesPrograma(
+        @RequestParam("programa") Integer idPrograma, Integer anoIni, Integer anoFin){
+        try{
+            List <Producao> producoes = programa.obterProducoes(idPrograma, anoIni, anoFin);
+            return new ResponseEntity(producoes, HttpStatus.OK);
+        }catch (ServicoRuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/obterOrientacoesPrograma")
+    public ResponseEntity obterOrientacoesPorgrama(
+        @RequestParam("programa") Integer idPrograma, Integer anoIni, Integer anoFin){
+        try{
+            List <Orientacao> orientacoes = programa.obterOrientacoes(idPrograma, anoIni, anoFin);
+            return new ResponseEntity(orientacoes, HttpStatus.OK);
+        }catch (ServicoRuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/obterTecnicasPrograma")
+    public ResponseEntity obterTecnicasPrograma(
+        @RequestParam("programa") Integer idPrograma, Integer anoIni, Integer anoFin){
+        try{
+            List <Tecnica> tecnicas = programa.obterTecnicas(idPrograma, anoIni, anoFin);
+            return new ResponseEntity(tecnicas, HttpStatus.OK);
+        }catch (ServicoRuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    ////////////////
+
     @GetMapping("/qtv_orientacoes_producao") // QTV = quantitativo
     public ResponseEntity<?> ObterQuantitativoOrientacaoProducao(
                         @RequestParam("idPrograma") Integer idPrograma,
